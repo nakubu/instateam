fdc1ad93a29d5f2b0ded7bdbba70b9ee200d379c
Oleksii Duniushkin <nakubu@gmail.com>
2024-10-25 17:07:57 -0700

    client validation

--- client/src/lib/util.ts
+++ client/src/lib/util.ts
@@ -9,3 +9,28 @@ export function getMember(formData: FormData): Member {
     role: formData.get('role') as 'regular' | 'admin',
   };
 }
+
+function isEmailValid(email: string) {
+  const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+  return emailPattern.test(email);
+}
+
+function isPhoneValid(phone: string) {
+  const phonePattern = /^\d{3}-\d{3}-\d{4}$/;
+  return phonePattern.test(phone);
+}
+
+export function validateForm(formData: FormData) {
+  const errors: Record<string, string> = {};
+  if (!isEmailValid(formData.get('email') as string)) {
+    errors.email = 'Invalid email address';
+  }
+  if (!isPhoneValid(formData.get('phone') as string)) {
+    errors.phone = 'Invalid phone number';
+  }
+  if (Object.keys(errors).length > 0) {
+    return errors;
+  } else {
+    return null;
+  }
+}
