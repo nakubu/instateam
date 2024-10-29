6e86ed6412e2665c419563f84793e707a7e66aa7
Oleksii Duniushkin <nakubu@gmail.com>
2024-10-28 19:59:12 -0700

    unique phone

--- server/api/models.py
+++ server/api/models.py
@@ -4,7 +4,7 @@ class Member(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
-    phone = models.CharField(max_length=15, blank=True)
+    phone = models.CharField(max_length=15, blank=True, unique=True)
     role = models.CharField(max_length=100)
 
     def __str__(self):
